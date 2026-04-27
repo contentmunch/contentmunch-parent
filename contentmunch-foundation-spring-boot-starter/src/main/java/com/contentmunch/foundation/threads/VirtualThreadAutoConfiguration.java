72b5c12b4b48997a73e8c1cb9a4b84bc4421b7a2
contentmuncher <890395+asikpradhan@users.noreply.github.com>
2026-04-26 20:57:13 -0400

    feat: make my executor service primary

--- contentmunch-foundation-spring-boot-starter/src/main/java/com/contentmunch/foundation/threads/VirtualThreadAutoConfiguration.java
+++ contentmunch-foundation-spring-boot-starter/src/main/java/com/contentmunch/foundation/threads/VirtualThreadAutoConfiguration.java
@@ -6,11 +6,13 @@ import java.util.concurrent.Executors;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ConditionalOnMissingBean(name = "virtualThreadExecutor")
 public class VirtualThreadAutoConfiguration {
     @Bean
+    @Primary
     public ExecutorService virtualThreadExecutor(){
         return Executors.newVirtualThreadPerTaskExecutor();
     }
